eb0a9a2d693830d1455537021bc16ea1ec8bf34f
TheMiniTom Bury <theminitom59@hotmail.com>
2023-10-05 15:22:54 +0200

    petite modification et mot de passe caché

--- views/emails/aEmprunter.php
+++ views/emails/aEmprunter.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Remerciements pour l'emprunt</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Remerciements pour l'emprunt</title>
+        <script src="https://cdn.tailwindcss.com"></script>
 
-</head>
-<body class="bg-gray-100 font-sans">
-<div class="container mx-auto p-8">
-    <h1 class="text-3xl font-bold mb-4">Merci de nous faire confiance</h1>
+    </head>
 
-    <p class="text-lg">Nous tenons à vous remercier pour avoir emprunter <?= $titre ?> via notre médiathèque.</p>
-    <p class="mt-4">Nous espérons que vous apprécierez et que vous reviendrez bientôt.</p>
-    <p class="mt-4">À bientôt !</p>
-</div>
-</body>
+    <body class="bg-gray-100 font-sans">
+        <div class="container mx-auto p-8">
+            <h1 class="text-3xl font-bold mb-4">Merci de nous faire confiance</h1>
+
+            <p class="text-lg">Nous tenons à vous remercier pour avoir emprunter <?= $titre ?> via notre médiathèque.</p>
+            <p class="mt-4">Nous espérons que vous apprécierez et que vous reviendrez bientôt.</p>
+            <p class="mt-4">À bientôt !</p>
+        </div>
+    </body>
 </html>
+
+
+
+
+
+
--- views/emails/newAccount.php
+++ views/emails/newAccount.php
@@ -2,25 +2,28 @@
 
 
 <!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Bienvenue sur le site de la médiathèque</title>
-    <script src="https://cdn.tailwindcss.com"></script>
 
-</head>
-<body class="bg-gray-100 font-sans">
-<div class="container mx-auto p-8">
-    <h1 class="text-3xl font-bold mb-4">Bienvenue sur le site de la médiathèque</h1>
-    <p>Votre compte a bien été créé. Voici vos identifiants de connexion :</p>
-    <ul class="list-disc ml-8 mt-2">
-        <li><strong>Identifiant :</strong> <?= $email ?></li>
-        <li><strong>Mot de passe :</strong> <?= $password ?></li>
-    </ul>
-    <p class="mt-4">Pour finaliser votre inscription, merci de cliquer sur le lien ci-dessous :</p>
-    <p class="mt-2"><a href="<?= $url ?>" class="text-blue-500 hover:underline">Valider l'inscription</a></p>
-    <p class="mt-4">A bientôt sur notre site !</p>
-</div>
-</body>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Bienvenue sur le site de la médiathèque</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+
+    </head>
+
+    <body class="bg-gray-100 font-sans">
+        <div class="container mx-auto p-8">
+            <h1 class="text-3xl font-bold mb-4">Bienvenue sur le site de la médiathèque</h1>
+            <p>Votre compte a bien été créé. Voici vos identifiants de connexion :</p>
+            <ul class="list-disc ml-8 mt-2">
+                <li><strong>Identifiant :</strong> <?= $email ?></li>
+                <li><strong>Mot de passe :</strong>*********</li>
+            </ul>
+            <p class="mt-4">Pour finaliser votre inscription, merci de cliquer sur le lien ci-dessous :</p>
+            <p class="mt-2"><a href="<?= $url ?>" class="text-blue-500 hover:underline">Valider l'inscription</a></p>
+            <p class="mt-4">A bientôt sur notre site !</p>
+        </div>
+    </body>
+
 </html>
 
