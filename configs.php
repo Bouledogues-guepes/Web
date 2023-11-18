79cd103e834d3ae6379783f9bd78381c5d20ce58
tombury59 <tombury59@hotmail.com>
2023-11-18 13:55:23 +0100

    modif pour server

--- configs.php
+++ configs.php
@@ -5,7 +5,7 @@ $DB_DATABASE = getenv("MVC_DB") ?: "bouledogues-guepes";
 $DB_USER = getenv("MVC_USER") ?: "bouledogues-guepes-1";
 $DB_PASSWORD = getenv("MVC_TOKEN") ?: "QVvnfAPP";
 $DEBUG = getenv("MVC_DEBUG") ?: true;
-$URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "http://localhost:9000/valider-compte/";
+$URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "192.168.103.2/valider-compte/";
 $MAIL_SERVER = getenv("MVC_MAIL_SERVER") ?: "192.168.10.15";
 $FROM_EMAIL = getenv("MVC_FROM_EMAIL") ?: "contact@mediaatout.fr";
 
