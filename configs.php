cdb347e443a5f23ea2b15ce3fec087e34cc41b34
tombury <tombury59@hotmail.com>
2023-11-20 14:06:24 +0100

    bug3

--- configs.php
+++ configs.php
@@ -6,7 +6,7 @@ $DB_USER = getenv("MVC_USER") ?: "bouledogues-guepes-1";
 $DB_PASSWORD = getenv("MVC_TOKEN") ?: "QVvnfAPP";
 $DEBUG = getenv("MVC_DEBUG") ?: true;
 $URL_VALIDATION = getenv("MVC_URL_VALIDATION") ?: "192.168.103.2/valider-compte/";
-$MAIL_SERVER = getenv("MVC_MAIL_SERVER") ?: "192.168.10.15";
+$MAIL_SERVER = getenv("MVC_MAIL_SERVER") ?: "192.168.103.2";
 $FROM_EMAIL = getenv("MVC_FROM_EMAIL") ?: "contact@mediaatout.fr";
 
 return array(
