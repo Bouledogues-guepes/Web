51ec4e57db63e4a26c0c93a3df3146f76794dafa
tombury59 <tombury59@hotmail.com>
2023-11-23 00:40:36 +0100

    tentative bug catégorie3

--- views/catalogue/liste.php
+++ views/catalogue/liste.php
@@ -23,8 +23,8 @@
                         ?>
                         <li>
                             <div class="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                <input id="<?= $row->idcategorie?>" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                <label for="<?= $row->idcategorie?>" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"><?php echo $row->libellecategorie;?></label>
+                                <input id="<?= $row->idcategorie ?>" type="checkbox" name="selectedCategories[]" value="<?= $row->idcategorie ?>" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                <label for="<?= $row->idcategorie ?>" class="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300"><?php echo $row->libellecategorie; ?></label>
                             </div>
                         </li>
                     <?php } ?>
